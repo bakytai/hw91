54fd9a6d418b57c89033b33a96b85231954ba825
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-25 18:18:33 +0600

    прописала логику сервера

--- api/index.js
+++ api/index.js
@@ -1,55 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-const {nanoid} = require('nanoid');
+const { nanoid } = require('nanoid');
 const app = express();
-
 require('express-ws')(app);
-
 const port = 8000;
 
 app.use(cors());
 
 const activeConnections = {};
+const savedCoordinates = [];
 
-
-app.ws('/chat', (ws, req) => {
+app.ws('/draw', function (ws, req) {
     const id = nanoid();
-    console.log('client connected! id=', id);
     activeConnections[id] = ws;
-
-    let username = 'Anonymus';
-
+    ws.send(JSON.stringify({
+        type: 'PREV_PIXELS',
+        coordinates: savedCoordinates,
+    }));
+    ws.on('close', (msg) => {
+        delete activeConnections[id];
+    });
     ws.on('message', (msg) => {
         const decodedMessage = JSON.parse(msg);
         switch (decodedMessage.type) {
-            case 'SET_USERNAME':
-                username = decodedMessage.username;
-                break;
-            case 'SEND_MESSAGE':
-                Object.keys(activeConnections).forEach(id => {
-                    const conn = activeConnections[id];
+            case 'SEND_PIXEL':
+                Object.keys(activeConnections).forEach(conId => {
+                    const conn = activeConnections[conId];
+                    savedCoordinates.push(decodedMessage.coordinates)
                     conn.send(JSON.stringify({
-                        type: 'NEW_MESSAGE',
-                        message: {
-                            username,
-                            text: decodedMessage.text
-                        }
-                    }))
-                })
+                        type: 'NEW_PIXEL',
+                        pixelCoordinates: decodedMessage.coordinates
+                    }));
+                });
                 break;
             default:
                 console.log('Unknown message type:', decodedMessage.type);
         }
-    })
-
-    ws.on('close', () => {
-        console.log('client disconnected! id=', id);
-        delete activeConnections[id]
     });
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server started on ${port} port!`)
+    console.log(`Server started on ${port} port!`);
 });
