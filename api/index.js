45c308756f4a046878021369fc377eadd559f9e7
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-25 18:22:15 +0600

    прописала верстку

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const { nanoid } = require('nanoid');
 const app = express();
+
 require('express-ws')(app);
+
 const port = 8000;
 
 app.use(cors());
